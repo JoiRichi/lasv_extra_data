a7fb3eac8d4ae71b4d244105ca4cfd8e394b9a82
jakobriccabona <riccabona.jakob@gmail.com>
2025-11-18 15:39:48 +0100

    added fasta files in the right format; and structure prediction script (singularity image needed)

--- structures_predictor.py
+++ structures_predictor.py
@@ -3,28 +3,25 @@ import subprocess
 import time
 
 # Define the folder containing the FASTA files and other required paths
-fasta_folder = '/data/horse/ws/jari462g-structure-prediction/lassa-db/all_fasta'
-template_path = '/data/horse/ws/jari462g-structure-prediction/lassa-db/templates'
-output_folder = '/data/horse/ws/jari462g-structure-prediction/lassa-db/all_structures'
+fasta_folder = './all_fasta'
+template_path = './templates'
+output_folder = './all_structures'
 os.makedirs(output_folder, exist_ok=True)
 
 # Loop through all files in the 'all_fasta' folder
 for filename in os.listdir(fasta_folder):
-    if filename.endswith('.fasta'):  # Check if the file is a FASTA file
+    if filename.endswith('.fasta'):
         fasta_file_path = os.path.join(fasta_folder, filename)
         
-        # Define the output folder based on the input file
         output_folder_for_file = os.path.join(output_folder, filename.replace('_processed.fasta', ''))
 
-        # Check if output folder has any .pdb files
         pdb_files_exist = (
             os.path.exists(output_folder_for_file) and
             any(f.endswith('.pdb') for f in os.listdir(output_folder_for_file))
         )
 
-        # Construct the command to run
         command = [
-            'sbatch', '/data/horse/ws/jari462g-structure-prediction/run_colabfold.job',
+            'sbatch', './YOUR_RUN_SCRIPT_OF_COLABFOLD.job',
             '-i', fasta_file_path,
             '-o', output_folder_for_file,
             '--', '--templates',
@@ -33,7 +30,7 @@ for filename in os.listdir(fasta_folder):
             '--use-gpu-relax',
         ]
         
-        # Only run if no PDB files are found
+        # if jobs fail
         if not pdb_files_exist:
             try:
                 print(f"Submitting job for {filename}...")
